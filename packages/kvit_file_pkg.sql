2e63f6289ac05808ebf31b9060966e4cb2e417ab
alex <avtor_house@mail.ru>
2021-07-10 12:01:32 +0300

    first commit

---
+++ packages/kvit_file_pkg.sql
@@ -0,0 +1,8 @@
+﻿create or replace package kvit_file_pkg is
+
+  -- Author  : ALEX
+  -- Created : 10.07.2021 11:15:32
+  -- Purpose : Работа с файлами
+
+end kvit_file_pkg;
+/
---
+++ tables/kvit_file_t.sql
@@ -0,0 +1,5 @@
+﻿create table kvit_file_t 
+(
+file_id number,
+file_name varchar2(255)
+);
