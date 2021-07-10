9320048e682b51cd454c72717def8c00734047aa
alex <avtor_house@mail.ru>
2021-07-10 12:56:33 +0300

    Рефакторинг и новый функционал

--- package bodies/kvit_file_pkg_body.sql
+++ package bodies/kvit_file_pkg_body.sql
@@ -3,6 +3,11 @@
   -- Author  : ALEX
   -- Created : 10.07.2021 11:15:32
   -- Purpose : Работа с файлами
+
+  procedure test is
+  begin
+    dbms_output.put_line('test procedure');
+  end;
   
 end kvit_file_pkg;
 /
